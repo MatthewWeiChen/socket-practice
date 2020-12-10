fe9a45debcd4178c69eded2034445d2f954be8dd
MatthewWeiChen <mattchen1021@gmail.com>
2020-12-10 14:21:50 -0800

    completed basic chat.js

---
+++ video-code/socketio101/chat.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const socketio = require("socket.io");
+
+app.use(express.static(__dirname + "/public"));
+
+const expressServer = app.listen(8000);
+
+const io = socketio(expressServer);
+io.on("connection", (socket) => {
+  socket.emit("messageFromServer", { data: "Welcome to the socketio sever" });
+  socket.on("messageToServer", (dataFromClient) => {
+    console.log(dataFromClient);
+  });
+});
