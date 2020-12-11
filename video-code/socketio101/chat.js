bef571681fe127dc992c6aba763728b8333d18e0
MatthewWeiChen <mattchen1021@gmail.com>
2020-12-10 16:12:44 -0800

    created basic chat

--- video-code/socketio101/chat.js
+++ video-code/socketio101/chat.js
@@ -7,9 +7,16 @@ app.use(express.static(__dirname + "/public"));
 const expressServer = app.listen(8000);
 
 const io = socketio(expressServer);
+
 io.on("connection", (socket) => {
   socket.emit("messageFromServer", { data: "Welcome to the socketio sever" });
+
   socket.on("messageToServer", (dataFromClient) => {
     console.log(dataFromClient);
+  });
+
+  socket.on("newMessageToServer", (msg) => {
+    //console.log(msg);
+    io.emit("messageToClients", { text: msg.text });
   });
 });
