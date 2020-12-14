04a399de356ba827df726529eacb422041ad4ce3
MatthewWeiChen <mattchen1021@gmail.com>
2020-12-14 11:34:44 -0800

    joined mainns and updated dom

--- video-code/socketio201/slack/public/script.js
+++ video-code/socketio201/slack/public/script.js
@@ -16,7 +16,15 @@ socket.on("nsList", (nsData) => {
   let namespacesDiv = document.querySelector(".namespaces");
   namespacesDiv.innerHTML = "";
   nsData.forEach((ns) => {
-    namespacesDiv.innerHTML += `<div class="namespace"><img src="${ns.img}" /></div>`;
+    namespacesDiv.innerHTML += `<div class="namespace" ns=${ns.endpoint}><img src="${ns.img}" /></div>`;
+  });
+  //set up click listener to each namespace
+
+  Array.from(document.getElementsByClassName("namespace")).forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+      const nsEndpoint = elem.getAttribute("ns");
+      console.log(nsEndpoint);
+    });
   });
 });
 
