8f54a619742e0ada38df5b99a7693a7b97932bc6
MatthewWeiChen <mattchen1021@gmail.com>
2020-12-15 11:02:59 -0800

    added emit msgs to client and server

--- video-code/socketio201/slack/public/joinRoom.js
+++ video-code/socketio201/slack/public/joinRoom.js
@@ -1,1 +1,8 @@
-function joinRoom() {}
+function joinRoom(roomName) {
+  nsSocket.emit("joinRoom", roomName, (newNumberOfMembers) => {
+    //we want to update the room member total now that we have joined!
+    document.querySelector(
+      ".curr-room-num-users"
+    ).innerHTML = `${newNumberOfMembers} <span class="glyphicon glyphicon-user"></span>`;
+  });
+}
