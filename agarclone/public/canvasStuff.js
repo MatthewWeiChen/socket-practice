81f3d6f2d37369527ce8772cacb98054aa9494d4
MatthewWeiChen <mattchen1021@gmail.com>
2020-12-22 23:39:27 -0800

    added circle

---
+++ agarclone/public/canvasStuff.js
@@ -0,0 +1,68 @@
+function init(){
+  draw();
+}
+
+player.locX= Math.floor(500*Math.random() + 100);
+player.locY = Math.floor(500*Math.random() + 100);
+
+function draw(){
+  //clear the screen out so the old stuff is gone from the last frame
+  context.clearRect(0,0,canvas.width,canvas.height);
+  //reset the translation back to default
+  context.setTransform(1,0,0,1,0,0)
+
+  //clamp the camera to the player
+  const camX = -player.locX + canvas.width/2
+  const camY = -player.locX + canvas.height/2
+
+  //translate allows us to move the canvas around
+  context.translate(camX,camY)
+
+  context.beginPath();
+  context.fillStyle = 'rgb(255,0,0)'
+  //arg1,2 = x,y of the center of the arc
+  //arg 3 = radius
+  //arg 4 = where to start on the circle in radians, 0 = 3:00
+  //arg 5 = where to stop in radians
+  context.arc(player.locX, player.locY, 10,0,Math.PI*2)
+  context.fill();
+  context.lineWidth = 3;
+  context.strokeStyle = 'rgb(0,255,0)';
+  context.stroke();
+  requestAnimationFrame(draw);
+}
+
+canvas.addEventListener('mousemove',(event)=>{
+    console.log(event)
+    const mousePosition = {
+        x: event.clientX,
+        y: event.clientY
+    };
+    const angleDeg = Math.atan2(mousePosition.y - (canvas.height/2), mousePosition.x - (canvas.width/2)) * 180 / Math.PI;
+    if(angleDeg >= 0 && angleDeg < 90){
+        xVector = 1 - (angleDeg/90);
+        yVector = -(angleDeg/90);
+    }else if(angleDeg >= 90 && angleDeg <= 180){
+        xVector = -(angleDeg-90)/90;
+        yVector = -(1 - ((angleDeg-90)/90));
+    }else if(angleDeg >= -180 && angleDeg < -90){
+        xVector = (angleDeg+90)/90;
+        yVector = (1 + ((angleDeg+90)/90));
+    }else if(angleDeg < 0 && angleDeg >= -90){
+        xVector = (angleDeg+90)/90;
+        yVector = (1 - ((angleDeg+90)/90));
+    }
+
+    speed = 10
+    xV = xVector;
+    yV = yVector;
+
+    if((player.locX < 5 && player.xVector < 0) || (player.locX > 500) && (xV > 0)){
+        player.locY -= speed * yV;
+    }else if((player.locY < 5 && yV > 0) || (player.locY > 500) && (yV < 0)){
+        player.locX += speed * xV;
+    }else{
+        player.locX += speed * xV;
+        player.locY -= speed * yV;
+    }
+})
