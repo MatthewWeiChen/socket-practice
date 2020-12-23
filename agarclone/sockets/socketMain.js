fb6953d7a0d312e14043a3586b26dfe7987904e7
MatthewWeiChen <mattchen1021@gmail.com>
2020-12-23 11:44:40 -0800

    made orb class

--- agarclone/sockets/socketMain.js
+++ agarclone/sockets/socketMain.js
@@ -1,4 +1,8 @@
 //where all our main socket stuff will go
-const io = require('../servers').io
+const io = require("../servers").io;
+
+const Orb = require("./classes/Orbs");
+
+function initGame() {}
 
 module.exports = io;
